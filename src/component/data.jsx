d3e92ba4bfa30d545db5d0638764c1e4dc13d108
SunnyInstitute <sunnyinstitute1999@gmail.com>
2024-01-17 20:12:07 +0530

    uploading kbc

---
+++ src/component/data.jsx
@@ -0,0 +1,125 @@
+export var price=["1000","3000","5000","10,000","20,000","40,000","80,000","1,60,000","3,20,000","6,40,000","12,80,000","25,00000","50,00000","1 Crore","2 Crores"]
+
+
+export var quiz=[
+    {
+        "ques":"What is the capital city of India?",
+        "option1":"Delhi",
+        "option2":"Mumbai",
+        "option3":"Pune",
+        "option4":"Bhopal",
+        "answer":"Delhi"
+    },
+    {
+        "ques":"Which river is known as the Ganga in India?",
+        "option1":"Yamuna",
+        "option2":"Bhramaputra",
+        "option3":"Indus",
+        "option4":"Ganges",
+        "answer":"Ganges"
+    },
+    {
+        "ques":"Who was the first prime minister of India?",
+        "option1":"Jawarlal nehru",
+        "option2":"Mahatma gandhi",
+        "option3":"Indra Gandhi",
+        "option4":"Lal Bahadur Shastri",
+        "answer":"Jawarlal nehru"
+    },
+    {
+        "ques":"Which  historical monument is knows as a simbol of  love",
+        "option1":"taj mahal",
+        "option2":"red fort",
+        "option3":"qutub minar ",
+        "option4":"hawa mahal",
+        "answer":"taj mahal"
+    },
+    {
+        "ques":"Which  fesstival is   know as a festival of light?",
+        "option1":"diwali",
+        "option2":"holi",
+        "option3":"naavraatrri",
+        "option4":"Ganges",
+        "answer":"diwali"
+    },
+    {
+        "ques":"Who is the king of Fruits?",
+        "option1":"Apple",
+        "option2":"Guava",
+        "option3":"Dragon Fruit",
+        "option4":"Mango",
+        "answer":"Mango"
+    },
+    {
+        "ques":"What is the capital city of India?",
+        "option1":"Delhi",
+        "option2":"Mumbai",
+        "option3":"Pune",
+        "option4":"Bhopal",
+        "answer":"Delhi"
+    },
+    {
+        "ques":"Which river is known as the Ganga in India?",
+        "option1":"Yamuna",
+        "option2":"Bhramaputra",
+        "option3":"Indus",
+        "option4":"Ganges",
+        "answer":"Ganges"
+    },
+    {
+        "ques":"Who was the first prime minister of India?",
+        "option1":"Jawarlal nehru",
+        "option2":"Mahatma gandhi",
+        "option3":"Indra Gandhi",
+        "option4":"Lal Bahadur Shastri",
+        "answer":"Jawarlal nehru"
+    },
+    {
+        "ques":"What is the capital city of India?",
+        "option1":"Delhi",
+        "option2":"Mumbai",
+        "option3":"Pune",
+        "option4":"Bhopal",
+        "answer":"Delhi"
+    },
+    {
+        "ques":"Which river is known as the Ganga in India?",
+        "option1":"Yamuna",
+        "option2":"Bhramaputra",
+        "option3":"Indus",
+        "option4":"Ganges",
+        "answer":"Ganges"
+    },
+    {
+        "ques":"Who was the first prime minister of India?",
+        "option1":"Jawarlal nehru",
+        "option2":"Mahatma gandhi",
+        "option3":"Indra Gandhi",
+        "option4":"Lal Bahadur Shastri",
+        "answer":"Jawarlal nehru"
+    },
+    {
+        "ques":"What is the capital city of India?",
+        "option1":"Delhi",
+        "option2":"Mumbai",
+        "option3":"Pune",
+        "option4":"Bhopal",
+        "answer":"Delhi"
+    },
+    {
+        "ques":"Which river is known as the Ganga in India?",
+        "option1":"Yamuna",
+        "option2":"Bhramaputra",
+        "option3":"Indus",
+        "option4":"Ganges",
+        "answer":"Ganges"
+    },
+    {
+        "ques":"Who was the first prime minister of India?",
+        "option1":"Jawarlal nehru",
+        "option2":"Mahatma gandhi",
+        "option3":"Indra Gandhi",
+        "option4":"Lal Bahadur Shastri",
+        "answer":"Jawarlal nehru"
+    }
+]
